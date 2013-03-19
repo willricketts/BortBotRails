ed39be3b50fb1645d0270d7f356ee9e4e2cdef3e
Will Ricketts <will@willricketts.com>
2013-03-18 21:04:44 -0400

    Hooray! BortBot's sequel

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BortBotRails::Application.config.secret_token = 'c1ac463ffb90ae15bbdce1744ac8baa7f450298ea7cf534ef28119ca3161ec091efad34a202d900d37c908a3dda32573df27f1f9b1fe6e6cba241c3bddfd0a5a'
