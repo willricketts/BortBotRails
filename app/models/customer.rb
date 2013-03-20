6942cda044cbf47768456870435b70336d534e74
Will Ricketts <will@willricketts.com>
2013-03-20 09:29:09 -0400

    Began model relationships

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,3 +1,6 @@
 class Customer < ActiveRecord::Base
   attr_accessible :email, :issue, :name, :phone
+  
+  has_many :issues, :through => :user_issue
+  has_many :customers
 end
--- app/models/issue.rb
+++ app/models/issue.rb
@@ -1,3 +1,5 @@
 class Issue < ActiveRecord::Base
   attr_accessible :issue_detail, :issue_title
+  
+  belongs_to :customer, :through => :user_issue
 end
